67d81aeaa0c7ae9fbb957dbb212627aa4092a155
usha.nandin <unandini17@gmail.com>
2023-03-07 19:51:21 +0530

    Home Page

--- PopConTime/src/app/pages/home/home.component.ts
+++ PopConTime/src/app/pages/home/home.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  movieBannerResult: any = [];
+  trendingMovieResult: any = [];
+  constructor(private service: MovieApiServiceService) {}
+  ngOnInit(): void {
+    this.movieBannerApiData();
+    this.trendingMovieApiData();
+  }
+  movieBannerApiData() {
+    this.service.bannerApiData().subscribe((result) => {
+      console.log(result, 'BannerResult#');
+      this.movieBannerResult = result.results;
+      // console.log(this.movieBannerResult, 'results#');
+    });
+  }
 
+  trendingMovieApiData() {
+    this.service.trendingApiData().subscribe((result) => {
+      console.log(result, 'trendingResult#');
+      this.trendingMovieResult = result.results;
+    });
+  }
 }
